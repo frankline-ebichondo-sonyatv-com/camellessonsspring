c60018e49d2392868f3fd0ad855d6faae39f7ae7
frankline-ebichondo-sonyatv-com <Frankline.Ebichondo@sonyatv.com>
2014-07-23 23:19:40 -0500

    Added observation for bean with 2 methods not working for camel bean transformation

--- src/main/java/com/sonyatv/camel/beans/AddHelloWorldBean.java
+++ src/main/java/com/sonyatv/camel/beans/AddHelloWorldBean.java
@@ -6,4 +6,13 @@ public class AddHelloWorldBean {
 		System.out.println("Message: " + message);
 		return "Most Hello: " + message;
 	}
+	
+	/*
+	 * Two methods within a class cannot work. Instead they act as input for each other thereby running the input into an endless loop
+	 * 
+	public String canItTwiceSimple(String message){
+		System.out.println("Message: " + message);
+		return "Most Hello: " + message;
+	}
+	*/
 }
